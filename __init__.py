f3abb13e0ba160416c1d138a5327fea833cf7c60
eloi <eloipereira@gmail.com>
2020-03-21 14:18:15 +0000

    Fix identation problems induced by atom editor

--- __init__.py
+++
@@ -1,2 +0,0 @@
-
-from robolapse import *
--- robolapse.py
+++ robolapse.py
@@ -12,15 +12,22 @@ span = 70 #cm
 span_ratio = 50 #step/cm
 
 length = span * span_ratio * excit_mode
+gpio_is_set = False
 
-gpio.setmode(gpio.BCM)
-gpio.setup(DIR,gpio.OUT)
-gpio.setup(STEP,gpio.OUT)
-gpio.setup(SWITCH,gpio.IN,pull_up_down=gpio.PUD_UP)
-gpio.output(DIR,CW)
+def setup_gpio():
+	global gpio_is_set
+	if not gpio_is_set:
+		gpio.setmode(gpio.BCM)
+		gpio.setup(DIR,gpio.OUT)
+		gpio.setup(STEP,gpio.OUT)
+		gpio.setup(SWITCH,gpio.IN,pull_up_down=gpio.PUD_UP)
+		gpio.output(DIR,CW)
+		gpio_is_set = True
 
 #GOTO - go to a specific location (normalized to the total length)
 def GOTO(location,speed=70.0):
+	global gpio_is_set
+	setup_gpio()
 	RTH(speed)
 	delta_t = 60.0 / (speed * span_ratio * excit_mode) - (excit_mode * 2.0/3.0 - 2.0/3.0)/length
 	try:
@@ -37,35 +44,38 @@ def GOTO(location,speed=70.0):
 	except KeyboardInterrupt:
 		print("Stop!")
 		gpio.cleanup()
+		gpio_is_set = False
 
 
 # RTH - Return to home
 def RTH(speed=70.0):
+	global gpio_is_set
+	setup_gpio()
 	delta_t = 60.0 / (speed * span_ratio * excit_mode) - (excit_mode * 2.0/3.0 - 2.0/3.0)/length
 	try:
 		if gpio.input(SWITCH) == False:
 			at_home = True
 		else:
 			at_home = False
-		sleep(1)
-		while not at_home:
-			gpio.output(DIR,CCW)
-			gpio.output(STEP,gpio.HIGH)
-			sleep(delta_t/2.0)
-			gpio.output(STEP,gpio.LOW)
-			sleep(delta_t/2.0)
-			if gpio.input(SWITCH) == False:
-				at_home = True
 			sleep(1)
-		while at_home:
-			gpio.output(DIR,CW)
-			gpio.output(STEP,gpio.HIGH)
-			sleep(delta_t/2.0)
-			gpio.output(STEP,gpio.LOW)
-			sleep(delta_t/2.0)
-			if gpio.input(SWITCH) == True:
-				at_home = False
-
+			while not at_home:
+				gpio.output(DIR,CCW)
+				gpio.output(STEP,gpio.HIGH)
+				sleep(delta_t/2.0)
+				gpio.output(STEP,gpio.LOW)
+				sleep(delta_t/2.0)
+				if gpio.input(SWITCH) == False:
+					at_home = True
+					sleep(1)
+			while at_home:
+				gpio.output(DIR,CW)
+				gpio.output(STEP,gpio.HIGH)
+				sleep(delta_t/2.0)
+				gpio.output(STEP,gpio.LOW)
+				sleep(delta_t/2.0)
+				if gpio.input(SWITCH) == True:
+					at_home = False
 	except KeyboardInterrupt:
 		print("Stop!")
 		gpio.cleanup()
+		gpio_is_set = False
