fd46e686531d8cf1f28666649c7de991a5e6ed8d
eloi <eloipereira@gmail.com>
2020-03-25 20:13:29 +0000

    MOVE api and threading for non-blocking api calls

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import robolapse as rl
+import threading
 from flask import Flask, escape, request
 
 app = Flask(__name__)
@@ -10,15 +11,35 @@ def index():
 @app.route('/api/rth', methods=['GET'])
 def RTH():
     speed = request.args.get("speed")
-    rl.RTH(float(speed))
-    return "Return to home completed"
+    th = threading.Thread(target=rl.RTH, args=[float(speed)])
+    th.daemon=True
+    th.start()
+    return "Returning to home at " + speed + " cm/min"
 
 @app.route('/api/goto', methods=['GET'])
 def GOTO():
     loc = request.args.get("location")
     speed = request.args.get("speed")
-    rl.GOTO(float(loc),float(speed))
-    return "GOTO completed"
+    th = threading.Thread(target=rl.GOTO, args = [float(loc),float(speed)])
+    th.daemon=True
+    th.start()
+    return "Going to " + loc + " cm at " + speed + " cm/min"
+
+@app.route('/api/move', methods=['GET'])
+def MOVE():
+    length = request.args.get("length")
+    speed = request.args.get("speed")
+    direction = request.args.get("direction")
+    if direction == "right":
+        dir = 0
+    elif direction == "left":
+        dir = 1
+    th = threading.Thread(target=rl.MOVE, args = [float(length),float(speed),dir])
+    th.daemon=True
+    th.start()
+
+    return "Moving " + length + " cm to the " + direction + " at " + speed + " cm/min"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.jinja_env.cache = {}
+    app.run(debug=False, port=80, host='0.0.0.0')
--- robolapse.py
+++ robolapse.py
@@ -3,6 +3,7 @@ import RPi.GPIO as gpio
 import math
 import configparser as cp
 
+
 # Config
 config = cp.ConfigParser()
 config.read('config.ini')
@@ -19,34 +20,37 @@ CCW = 0
 gpio_is_set = False
 
 def setup_gpio():
+	gpio.setmode(gpio.BCM)
+	gpio.setup(DIR,gpio.OUT)
+	gpio.setup(STEP,gpio.OUT)
+	gpio.setup(SWITCH,gpio.IN,pull_up_down=gpio.PUD_UP)
+	gpio.output(DIR,CW)
+
+#GOTO - go to a specific location in cm
+def GOTO(location=SPAN,speed=70.0):
+	RTH(speed)
+	norm_location = float(location)/SPAN
 	global gpio_is_set
 	if not gpio_is_set:
-		gpio.setmode(gpio.BCM)
-		gpio.setup(DIR,gpio.OUT)
-		gpio.setup(STEP,gpio.OUT)
-		gpio.setup(SWITCH,gpio.IN,pull_up_down=gpio.PUD_UP)
-		gpio.output(DIR,CW)
+		setup_gpio()
 		gpio_is_set = True
-
-#GOTO - go to a specific location (normalized to the total length)
-def GOTO(location,speed=70.0):
-	global gpio_is_set
-	setup_gpio()
-	RTH(speed)
 	delta_t = 60.0 / (speed * SPAN_RATIO * STEP_MODE) - (STEP_MODE * 2.0/3.0 - 2.0/3.0)/LENGTH
 	try:
 		sleep(1)
 		direction = CW
-		for l in range(int(math.floor(location*LENGTH))):
-			gpio.output(DIR,direction)
+		gpio.output(DIR,direction)
+		for l in range(int(math.floor(norm_location*LENGTH))):
 			gpio.output(STEP,gpio.HIGH)
 			sleep(delta_t/2.0)
 			gpio.output(STEP,gpio.LOW)
 			sleep(delta_t/2.0)
 			if (l+1)%LENGTH == 0:
 				direction = (direction + 1)%2
+				gpio.output(DIR,direction)
 	except KeyboardInterrupt:
 		print("Stop!")
+		pass
+	finally:
 		gpio.cleanup()
 		gpio_is_set = False
 
@@ -54,7 +58,9 @@ def GOTO(location,speed=70.0):
 # RTH - Return to home
 def RTH(speed=70.0):
 	global gpio_is_set
-	setup_gpio()
+	if not gpio_is_set:
+		setup_gpio()
+		gpio_is_set = True
 	delta_t = 60.0 / (speed * SPAN_RATIO * STEP_MODE) - (STEP_MODE * 2.0/3.0 - 2.0/3.0)/LENGTH
 	try:
 		if gpio.input(SWITCH) == False:
@@ -62,17 +68,17 @@ def RTH(speed=70.0):
 		else:
 			at_home = False
 			sleep(1)
+			gpio.output(DIR,CCW)
 			while not at_home:
-				gpio.output(DIR,CCW)
 				gpio.output(STEP,gpio.HIGH)
 				sleep(delta_t/2.0)
 				gpio.output(STEP,gpio.LOW)
 				sleep(delta_t/2.0)
 				if gpio.input(SWITCH) == False:
 					at_home = True
-					sleep(1)
+			sleep(1)
+			gpio.output(DIR,CW)
 			while at_home:
-				gpio.output(DIR,CW)
 				gpio.output(STEP,gpio.HIGH)
 				sleep(delta_t/2.0)
 				gpio.output(STEP,gpio.LOW)
@@ -81,5 +87,30 @@ def RTH(speed=70.0):
 					at_home = False
 	except KeyboardInterrupt:
 		print("Stop!")
+		pass
+	finally:
+		gpio.cleanup()
+		gpio_is_set = False
+
+
+# MOVE - Move at a given speed and direction
+def MOVE(length = 0.0,speed=70.0,direction=0):
+	norm_length = float(length)/SPAN
+	global gpio_is_set
+	setup_gpio()
+	delta_t = 60.0 / (speed * SPAN_RATIO * STEP_MODE) - (STEP_MODE * 2.0/3.0 - 2.0/3.0)/LENGTH
+	try:
+		sleep(1)
+		gpio.output(DIR,direction)
+		for i in range(int(math.floor(norm_length*LENGTH))):
+		       	gpio.output(STEP,gpio.HIGH)
+		       	sleep(delta_t/2.0)
+	       		gpio.output(STEP,gpio.LOW)
+       			sleep(delta_t/2.0)
+
+	except KeyboardInterrupt:
+		print("Stop!")
+		pass
+	finally:
 		gpio.cleanup()
 		gpio_is_set = False
